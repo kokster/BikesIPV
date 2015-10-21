0fa8c667f90ff06c7306ef3339bf33fcd1223b0e
Preslav <shadow358@abv.bg>
2015-10-21 18:22:41 +0200

    Added Tutorial part

--- BikesIPV/Form1.Designer.cs
+++ BikesIPV/Form1.Designer.cs
@@ -39,27 +39,35 @@ namespace BikesIPV
             this.button3 = new System.Windows.Forms.Button();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.button4 = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.problems = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btn_next = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btn_help = new System.Windows.Forms.Button();
+            this.pb_tutorial = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_tutorial)).BeginInit();
             this.SuspendLayout();
             // 
             // imageBox1
             // 
-            this.imageBox1.Location = new System.Drawing.Point(10, 11);
-            this.imageBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.imageBox1.Location = new System.Drawing.Point(13, 14);
+            this.imageBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.imageBox1.Name = "imageBox1";
-            this.imageBox1.Size = new System.Drawing.Size(593, 455);
+            this.imageBox1.Size = new System.Drawing.Size(791, 560);
             this.imageBox1.TabIndex = 2;
             this.imageBox1.TabStop = false;
             // 
             // imageBox2
             // 
-            this.imageBox2.Location = new System.Drawing.Point(698, 11);
-            this.imageBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.imageBox2.Location = new System.Drawing.Point(931, 14);
+            this.imageBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.imageBox2.Name = "imageBox2";
-            this.imageBox2.Size = new System.Drawing.Size(560, 455);
+            this.imageBox2.Size = new System.Drawing.Size(747, 560);
             this.imageBox2.TabIndex = 2;
             this.imageBox2.TabStop = false;
             // 
@@ -69,16 +77,18 @@ namespace BikesIPV
             this.leftRightCombo.Items.AddRange(new object[] {
             "Left",
             "Right"});
-            this.leftRightCombo.Location = new System.Drawing.Point(6, 19);
+            this.leftRightCombo.Location = new System.Drawing.Point(8, 23);
+            this.leftRightCombo.Margin = new System.Windows.Forms.Padding(4);
             this.leftRightCombo.Name = "leftRightCombo";
-            this.leftRightCombo.Size = new System.Drawing.Size(235, 21);
+            this.leftRightCombo.Size = new System.Drawing.Size(312, 24);
             this.leftRightCombo.TabIndex = 3;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(6, 46);
+            this.button1.Location = new System.Drawing.Point(8, 57);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(235, 23);
+            this.button1.Size = new System.Drawing.Size(313, 28);
             this.button1.TabIndex = 4;
             this.button1.Text = "Use the camera";
             this.button1.UseVisualStyleBackColor = true;
@@ -90,18 +100,21 @@ namespace BikesIPV
             this.groupBox1.Controls.Add(this.comboBox2);
             this.groupBox1.Controls.Add(this.leftRightCombo);
             this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Location = new System.Drawing.Point(236, 487);
+            this.groupBox1.Location = new System.Drawing.Point(315, 599);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(451, 84);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(601, 103);
             this.groupBox1.TabIndex = 5;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Input";
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(247, 46);
+            this.button3.Location = new System.Drawing.Point(329, 57);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(198, 23);
+            this.button3.Size = new System.Drawing.Size(264, 28);
             this.button3.TabIndex = 6;
             this.button3.Text = "Use selected picture";
             this.button3.UseVisualStyleBackColor = true;
@@ -110,44 +123,119 @@ namespace BikesIPV
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(247, 19);
+            this.comboBox2.Location = new System.Drawing.Point(329, 23);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(4);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(198, 21);
+            this.comboBox2.Size = new System.Drawing.Size(263, 24);
             this.comboBox2.TabIndex = 5;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(859, 488);
+            this.button4.Location = new System.Drawing.Point(1145, 601);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(321, 83);
+            this.button4.Size = new System.Drawing.Size(428, 102);
             this.button4.TabIndex = 7;
             this.button4.Text = "do your thing!";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
-            // panel1
+            // problems
             // 
-            this.panel1.Location = new System.Drawing.Point(677, 577);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(591, 454);
-            this.panel1.TabIndex = 8;
+            this.problems.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.problems.FormattingEnabled = true;
+            this.problems.Items.AddRange(new object[] {
+            "Flat Front Tire",
+            "Flat Back Tire",
+            "Crank Replacement"});
+            this.problems.Location = new System.Drawing.Point(160, 768);
+            this.problems.Name = "problems";
+            this.problems.Size = new System.Drawing.Size(188, 24);
+            this.problems.TabIndex = 8;
+            this.problems.SelectedIndexChanged += new System.EventHandler(this.problems_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 771);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "What is the problem?";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btn_next);
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Location = new System.Drawing.Point(380, 743);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(691, 282);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Fix It Yourself :)";
+            // 
+            // btn_next
+            // 
+            this.btn_next.Location = new System.Drawing.Point(593, 25);
+            this.btn_next.Name = "btn_next";
+            this.btn_next.Size = new System.Drawing.Size(91, 31);
+            this.btn_next.TabIndex = 1;
+            this.btn_next.Text = "Next";
+            this.btn_next.UseVisualStyleBackColor = true;
+            this.btn_next.Click += new System.EventHandler(this.btn_next_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Location = new System.Drawing.Point(18, 25);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(569, 173);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // btn_help
+            // 
+            this.btn_help.Location = new System.Drawing.Point(160, 811);
+            this.btn_help.Name = "btn_help";
+            this.btn_help.Size = new System.Drawing.Size(124, 23);
+            this.btn_help.TabIndex = 11;
+            this.btn_help.Text = "Help Me!";
+            this.btn_help.UseVisualStyleBackColor = true;
+            this.btn_help.Click += new System.EventHandler(this.btn_help_Click);
+            // 
+            // pb_tutorial
+            // 
+            this.pb_tutorial.Location = new System.Drawing.Point(1106, 710);
+            this.pb_tutorial.Name = "pb_tutorial";
+            this.pb_tutorial.Size = new System.Drawing.Size(383, 248);
+            this.pb_tutorial.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_tutorial.TabIndex = 12;
+            this.pb_tutorial.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1280, 584);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(1707, 1037);
+            this.Controls.Add(this.pb_tutorial);
+            this.Controls.Add(this.btn_help);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.problems);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.imageBox2);
             this.Controls.Add(this.imageBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
             this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_tutorial)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -161,7 +249,13 @@ namespace BikesIPV
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox problems;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btn_next;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btn_help;
+        private System.Windows.Forms.PictureBox pb_tutorial;
     }
 }
 
--- BikesIPV/Form1.cs
+++ BikesIPV/Form1.cs
@@ -29,6 +29,11 @@ namespace BikesIPV
         private bool captureInProgress = false;
         private int framecounter;
         BikeIPV bIPV;
+        private string[,] arrayProblems;
+        private int problemIndex;
+        private int stepIndex;
+        private int picIndex;
+
 
 
         public Form1()
@@ -40,6 +45,22 @@ namespace BikesIPV
             getMeTheResources();
             framecounter = 0;
             bIPV = new BikeIPV("Left");
+            //problems.Enabled = false;
+            btn_next.Enabled = false;
+            arrayProblems = new string[3,10];
+            arrayProblems[0, 0] = "Step1:Remove The Tire \n Hook the rounded end of one tire lever under the bead (the outer edge) of the tire to unseat it. Hook the other end onto a spoke to hold the lever in place and to keep the unseated tire from popping back into the rim. Hook the rounded end of the second lever under the bead next to the first and walk it around the tire/rim clockwise until one side of the tire is off the rim." ;
+            arrayProblems[0, 1] = "Step2:Find The Culprit \n Remove the tube and pump air into it to find the leak. Two holes side by side is a pinch-flat--the tube got pinched between the tire and rim. A single hole was most likely caused by a sharp object such as a thorn or a piece of glass. Carefully run your fingers along the inside of the tire to make sure the foreign object is no longer there. If you don't, it could cause another flat.";
+            arrayProblems[0, 2] = "Step3:If You Patch \n Clean the punctured area of the flat tire with an alcohol prep pad and rough the surface with an emery cloth. For a glueless patch, simply stick it over the hole and press firmly. For a patch that requires glue, apply a thin layer of glue to the tube and patch. Wait for the glue to get tacky, then apply the patch and press firmly until it adheres. If you don't patch, stuff the tube into your bag and fix it when you get home. It could be good for another season or more of use.";
+            arrayProblems[0, 3] = "Step4:Install the Tube \n Inflate either your patched or new tube until it holds its shape, then insert it into the tire. With the valve stem installed straight, work the tire back into the rim with your hands by rolling the bead away from yourself. (Do not use levers to reseat the tire, as you could puncture the tube.) When you get to the valve stem, tuck both sides of the tire bead low into the rim then push upward on the stem to get the tube up inside the tire. Inflate completely, checking that the bead is seated correctly.";
+            arrayProblems[1, 0] = "Step1:Remove The Tire \n Hook the rounded end of one tire lever under the bead (the outer edge) of the tire to unseat it. Hook the other end onto a spoke to hold the lever in place and to keep the unseated tire from popping back into the rim. Hook the rounded end of the second lever under the bead next to the first and walk it around the tire/rim clockwise until one side of the tire is off the rim.";
+            arrayProblems[1, 1] = "Step2:Find The Culprit \n Remove the tube and pump air into it to find the leak. Two holes side by side is a pinch-flat--the tube got pinched between the tire and rim. A single hole was most likely caused by a sharp object such as a thorn or a piece of glass. Carefully run your fingers along the inside of the tire to make sure the foreign object is no longer there. If you don't, it could cause another flat.";
+            arrayProblems[1, 2] = "Step3:If You Patch \n Clean the punctured area of the flat tire with an alcohol prep pad and rough the surface with an emery cloth. For a glueless patch, simply stick it over the hole and press firmly. For a patch that requires glue, apply a thin layer of glue to the tube and patch. Wait for the glue to get tacky, then apply the patch and press firmly until it adheres. If you don't patch, stuff the tube into your bag and fix it when you get home. It could be good for another season or more of use.";
+            arrayProblems[1, 3] = "Step4:Install the Tube \n Inflate either your patched or new tube until it holds its shape, then insert it into the tire. With the valve stem installed straight, work the tire back into the rim with your hands by rolling the bead away from yourself. (Do not use levers to reseat the tire, as you could puncture the tube.) When you get to the valve stem, tuck both sides of the tire bead low into the rim then push upward on the stem to get the tube up inside the tire. Inflate completely, checking that the bead is seated correctly.";
+            arrayProblems[2, 0] = "Step1:Unscrew The Bolt/nut \n Depending on the fitting of the square tapered crank bolt or nut, use either a socket or hex key to unscrew it. Make sure you remove any washer that’s under the head of the bolt.";
+            arrayProblems[2, 1] = "Step2:Retract The Bolt From The Crank-puller \n A crank-puller is a unit that screws into the threads on the inside of the square tapered crank holes. A bolt that runs through it that jams against the axle to force the square tapered crank off the tapered axle. Retract the bolt from the crank puller. Almost all square tapered cranks use the Italian standard 22mm thread but some old French ‘Stronglight’ or ‘T.A.’ cranks need a different crank-puller.";
+            arrayProblems[2, 2] = "Step3:Mount The Crank-puller \n Check the threads on the inside of the square tapered crank hole are clean and apply a smear of grease. Screw the crank-puller into the threads, do this carefully as the threads on the aluminium square tapered crank are soft and easily damaged. Finger strength is usually enough.";
+            arrayProblems[2, 3] = "Step4:Drive The Bolt into the square tapered crank \n When the crank-puller is fully in the square tapered crank, screw in the bolt on the crank-puller, it will jam against the axle and force the crank off the taper.";
+            arrayProblems[2, 4] = "Step5:Remove The Tool And Repeat \n Unscrew the tool from thesquare tapered crank arm and repeat the operation on the other side.";
         }
 
         private void button4_Click(object sender, EventArgs e)
@@ -205,6 +226,53 @@ namespace BikesIPV
                 Image<Bgr, Byte> image = new Image<Bgr, Byte>(BikesIPV.Properties.Resources.bike_white2);//.Resize(500, 400, Emgu.CV.CvEnum.INTER.CV_INTER_LINEAR, true );
                 Image<Gray, Byte> imageReady = bIPV.init(image, imageBox1);
                 imageBox1.Image = imageReady;
+            }
+        }
+
+        private void problems_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+             
+        }
+
+        private void btn_help_Click(object sender, EventArgs e)
+        {
+            if (problems.Text == "Flat Front Tire")
+            {
+                problemIndex = 0;
+                stepIndex = 0;
+                richTextBox1.Text = arrayProblems[problemIndex, stepIndex];
+                pb_tutorial.Image = null;
+            }
+            else if (problems.Text == "Flat Back Tire")
+            {
+                problemIndex = 1;
+                stepIndex = 0;
+                richTextBox1.Text = arrayProblems[problemIndex, stepIndex];
+                pb_tutorial.Image = null;
+            }
+            else if (problems.Text == "Crank Replacement")
+            {
+                problemIndex = 2;
+                stepIndex = 0;
+                picIndex = 1;
+                richTextBox1.Text = arrayProblems[problemIndex, stepIndex];
+                // pb_tutorial.Image = BikesIPV.Properties.Resources.
+                pb_tutorial.Load(@"..\..\..\Resources\RC1.jpg");
+            }
+            btn_next.Enabled = true;
+            
+        }
+
+        private void btn_next_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            stepIndex++;
+            picIndex++;
+            richTextBox1.Text = arrayProblems[problemIndex, stepIndex];
+            if (picIndex <= 5 && problemIndex==2 )
+            {
+                pb_tutorial.Load(@"..\..\..\Resources\RC" + picIndex + ".jpg");
             }
         }
     }
